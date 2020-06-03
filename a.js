2bdf4b722c4e6c053ec9a95b8207ac93cbfcdd50
Shanxiaolin <Shanxiaolin163@163.com>
2020-06-03 20:47:21 +0800

    html

---
+++ a.js
@@ -0,0 +1,6 @@
+function a(){
+  console.log(2)
+  console.log(document.getElementById('summary').style.color)
+  document.getElementById('summary').style.color="red"
+  $('#div').css('display','block')
+}
